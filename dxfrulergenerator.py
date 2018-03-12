8158c9578c689e68cf73c638f3cb02438d7b7a2a
umluizlima <umluizlima@gmail.com>
2018-03-12 20:44:19 -0300

    Ruler becomes a class

--- dxfrulergenerator.py
+++ dxfrulergenerator.py
@@ -1,49 +1,53 @@
-#https://github.com/mozman/dxfwrite
+import os
 from dxfwrite import DXFEngine as dxf
 
+class Ruler:  
+    
+    def __init__(self, ruler_length, ruler_width=30, tick_width=.25):
+        self.length = ruler_length
+        self.width = ruler_width # ruler width
+        self.tick_width = tick_width # width of each engraved line
+        self.tick_offset = 5 - self.tick_width / 2
 
-'''
-Constantes
-'''
-ruler_width = 30 #largura da régua
-tick_width = .25 #largura de cada traço, em milimetros
-tick_offset = 5 - tick_width / 2
+    # creates a black layer and then draws the ruler's external shape
+    def _draw_ruler(self):
+        self.file.add_layer('CUT', color = 7)
+        self.file.add(dxf.rectangle([0, 0], 10 * self.length + 10, self.width, layer = 'CUT'))
+        return self
 
-'''
-Funções
-'''
-#Cria uma camada na cor preta e desenha o contorno da régua
-def drawRuler(ruler_length):
-    file.add_layer('CUT', color = 7)
-    file.add(dxf.rectangle([0, 0], 10 * ruler_length + 10, ruler_width, layer = 'CUT'))
+    # creates a blue layer and calls the method that draws milimiter lines once for every centimeter
+    def _draw_centimeter(self):
+        self.file.add_layer('SCAN', color = 5)
+        for cm in range(self.length + 1):
+            self._draw_milimeter(cm)
+        return self
 
-#Cria uma camada na cor azul e chama a função que desenha os milimetros pelo
-#número de vezes igual ao número de centímetros
-def drawCentimeter(ruler_length):
-    file.add_layer('SCAN', color = 5)
-    for cm in range(ruler_length + 1):
-        drawMilimeter(cm)
+    #Desenha cada marcação de um centímetro, com diferentes tamanhos para 0 e 5 milimetros
+    def _draw_milimeter(self, cm):
+        for mm in range(10):
+            x_pos = self.tick_offset + 10 * cm + mm
+            if mm == 0: # draws a longer line at centimeter start
+                y_pos = 20
+                self.file.add(dxf.text(str(cm), [x_pos - 1, 20], height = 2, rotation = 90, layer = 'SCAN'))
+            elif mm == 5: # draws middle sized line at half centimeter
+                y_pos = 25
+            else: # draws short line at every other milimeter
+                y_pos = 27.5
+            self.file.add(dxf.rectangle([x_pos, y_pos], self.tick_width, self.width - y_pos, layer = 'SCAN'))
+            if cm == self.length: break
+        return self
+    
+    def draw(self):
+        if not os.path.lexists(os.path.join('.', 'rulers')):
+            os.mkdir(os.path.join('.', 'rulers'))
+        self.file = dxf.drawing(os.path.join('.', 'rulers', 'ruler_%dcm.dxf' %self.length))
+        self.file.header['$INSUNITS'] = 4
+        self._draw_ruler()
+        self._draw_centimeter()
+        self.file.save()
+        return self
 
-#Desenha cada marcação de um centímetro, com diferentes tamanhos para 0 e 5 milimetros
-def drawMilimeter(cm):
-    for mm in range(10):
-        x_pos = tick_offset + 10 * cm + mm
-        if mm == 0: #desenha o traço mais comprido na marcação de cada centímetro
-            y_pos = 20
-            file.add(dxf.text(str(cm), [x_pos - 1, 20], height = 2, rotation = 90, layer = 'SCAN'))
-        elif mm == 5: #desenha o traço médio na marcação de cada meio centímetro
-            y_pos = 25
-        else: #desenha o traço curto na marcação de cada milimetro
-            y_pos = 27.5
-        file.add(dxf.rectangle([x_pos, y_pos], tick_width, ruler_width - y_pos, layer = 'SCAN'))
-        if cm == ruler_length: break
-
-'''
-Programa principal
-'''
-ruler_length = int(input('Comprimento da régua (cm): '))
-file = dxf.drawing('rulers/ruler_%dcm.dxf' %ruler_length)
-file.header['$INSUNITS'] = 4
-drawRuler(ruler_length)
-drawCentimeter(ruler_length)
-file.save()
+if __name__ == '__main__':
+    ruler_length = int(input('Ruler length (in centimeters): '))
+    ruler = Ruler(ruler_length)
+    ruler.draw()
